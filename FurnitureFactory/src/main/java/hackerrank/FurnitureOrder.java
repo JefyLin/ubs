be10aba1e8f63e5725a7bafb46f3dff953b1311c
JefyLin <115690965+JefyLin@users.noreply.github.com>
2022-10-13 13:52:53 +0700

    Update FurnitureOrder.java

--- FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
+++ FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
@@ -1,45 +1,61 @@
 package hackerrank;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class FurnitureOrder implements FurnitureOrderInterface {
     /**
      * TODO: Create a map of Furniture items to order quantities
      */
+	
+	private HashMap<Furniture, Integer> orderedFurniture;
 
     /**
      * Initialize a new mapping of Furniture types to order quantities.
      */
     FurnitureOrder() {
         // TODO: Complete the constructor
+    	orderedFurniture = new HashMap<Furniture, Integer>();
     }
 
     public void addToOrder(final Furniture type, final int furnitureCount) {
         // TODO: Complete the method
+    	orderedFurniture.put(type, furnitureCount);
     }
 
     public HashMap<Furniture, Integer> getOrderedFurniture() {
         // TODO: Complete the method
-        return null;
+        return orderedFurniture;
     }
 
     public float getTotalOrderCost() {
         // TODO: Complete the method
-        return -1.0f;
+        float totalOrderCost = 0f;
+        
+    	for(Map.Entry<Furniture, Integer> furniture : this.orderedFurniture.entrySet()) {
+    		totalOrderCost += furniture.getValue() * furniture.getKey().cost();
+    	}
+    	
+    	return totalOrderCost;
     }
 
     public int getTypeCount(Furniture type) {
         // TODO: Complete the method
-        return -1;
+        return orderedFurniture.getOrDefault(type, 0);
     }
 
     public float getTypeCost(Furniture type) {
         // TODO: Complete the method
-        return -1.0f;
+        return getTypeCount(type) * type.cost();
     }
 
     public int getTotalOrderQuantity() {
         // TODO: Complete the method
-        return -1;
+    	int totalQuantity = 0;
+    	for(Map.Entry<Furniture, Integer> furniture : this.orderedFurniture.entrySet()) {
+    		totalQuantity += furniture.getValue();
+    	}
+    	
+    	return totalQuantity;
     }
 }
